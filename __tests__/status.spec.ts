eb3d9b69f566d7c44c6d2bc3fec1cf19a2415f48
Caio Frota <caiofrota@gmail.com>
2025-11-04 11:11:57 -0300

    test: fix status test

--- __tests__/status.spec.ts
+++ __tests__/status.spec.ts
@@ -1,8 +1,10 @@
+import { NextRequest } from "next/server";
 import { describe, expect, it } from "vitest";
-
+import { GET } from "app/api/v1/status/route";
 describe("Status API", () => {
   it("GET /api/v1/status", async () => {
-    const response = await fetch("http://localhost:3001/api/v1/status");
+    const request = new NextRequest("http://localhost/api/v1/status");
+    const response = await GET(request);
     const data = await response.json();
 
     expect(response.status).toBe(200);
